7b331cea4b0d603869d205b7869539302a340dae
Vyacheslav Pyshkin <slavapyshkin1@gmail.com>
2020-03-12 18:05:03 +0300

    Add timer (JS)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
 	'use strict';
 
+	//TABS
 	let tab = document.querySelectorAll('.info-header-tab'),
 		info = document.querySelector('.info-header'),
 		tabContent = document.querySelectorAll('.info-tabcontent');
@@ -34,5 +35,82 @@ window.addEventListener('DOMContentLoaded', function () {
 			}
 		}
 	});
+
+
+	//TIMER
+
+	let deadLine = '2020-05-01';
+
+	function getTimeRemaining(endtime) {
+
+		if (Date.parse(endtime) < Date.parse(new Date())) {
+			return {
+				'total': Date.parse(endtime) - Date.parse(new Date()),
+				'days': '00',
+				'hours': '00',
+				'minutes': '00',
+				'seconds': '00'
+			};
+		}
+		let t = Date.parse(endtime) - Date.parse(new Date()),
+			seconds = Math.floor((t / 1000) % 60),
+			minutes = Math.floor((t / 1000 / 60) % 60),
+			hours = Math.floor((t / 1000 / 60 / 60) % 24),
+			days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+
+
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	function setClock(id, endtime) {
+
+		let timer = document.getElementById(id),
+			days = timer.querySelector('.days'),
+			hours = timer.querySelector('.hours'),
+			minutes = timer.querySelector('.minutes'),
+			seconds = timer.querySelector('.seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+
+		function updateClock() {
+
+			let t = getTimeRemaining(endtime);
+			days.textContent = t.days;
+			if (days.textContent.length === 1) {
+				days.textContent = '0' + days.textContent;
+			}
+			hours.textContent = t.hours;
+			if (hours.textContent.length === 1) {
+				hours.textContent = '0' + hours.textContent;
+			}
+			minutes.textContent = t.minutes;
+			if (minutes.textContent.length === 1) {
+				minutes.textContent = '0' + minutes.textContent;
+			}
+			seconds.textContent = t.seconds;
+			if (seconds.textContent.length === 1) {
+				seconds.textContent = '0' + seconds.textContent;
+			}
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+
+	}
+
+	setClock('timer', deadLine);
+
+	// if (Date.parse(deadLine) > Date.parse(new Date())) {
+
+
+	// }
+
 
 });
